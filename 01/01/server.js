e59dbcf2563619a455d4de01d1ce0aba5f1b2d96
Marine King <mkp@prime.co.kr>
2015-08-17 11:01:47 -0300

    Servindo arquivos com cache

--- 01/01/server.js
+++ 01/01/server.js
@@ -8,33 +8,44 @@ var mimeTypes = {
     '.css' : 'text/css'
 };
 
+var cache = {};
+
+function cacheAndDeliver(f, cb){
+    
+    if (!cache[f]) {
+
+        fs.readFile(f, function (err, data) {
+            if (!err) {
+                cache[f] = {
+                    content : data,
+                    timestamp : Date.now()
+                };
+            }
+            cb(err, data);
+        });
+        return;
+    }
+    console.log('carregando ' + f + ' from cache');
+    cb(null, cache[f].content);
+}
+
 http.createServer(function (req, res) {
     
     var lookup = path.basename(decodeURI(req.url)) || 'index.html';
     var f = 'conteudo/' + lookup;
     
-    fs.exists(f, function (exists) {
-        
-        if (exists) {
-
-            fs.readFile(f, function (err, data) {
-                if (err) {
-                    res.writeHead(500);
-                    res.end('Server Error!');
-                    return;
-                }
-
-                var headers = { 'Content-type' : mimeTypes[path.extname(lookup)] };
-
-                res.writeHead(200, headers);
-                res.end(data);
-            });
+    cacheAndDeliver(f, function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            res.end('Server Error!');
             return;
         }
-
-        res.writeHead(404);
-        res.end();
-
-    });
+        
+        var headers = { 'Content-type' : mimeTypes[path.extname(lookup)] };
+        
+        res.writeHead(200, headers);
+        res.end(data);
+    } );
+    
     
 }).listen(9999);
