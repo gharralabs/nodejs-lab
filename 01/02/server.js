25c210b13ba06c803154a5d5886991297becf7f1
Marine King <mkp@prime.co.kr>
2015-08-20 15:36:19 -0300

    Processamento de Formulário - limitar o tamanho dos dados enviados pelo método  POST

--- 01/02/server.js
+++ 01/02/server.js
@@ -2,8 +2,9 @@
 var form = require('fs').readFileSync('form.html');
 var querystring = require('querystring');
 var util = require('util');
-
+var maxData = 2 * 1024 * 1024;
 var port = process.env.port || 1337;
+
 http.createServer(function (req, res) {
     
     
@@ -16,8 +17,20 @@ http.createServer(function (req, res) {
     if (req.method === 'POST') {
         var postData = '';
         req.on('data', function (chunk) {
-            postData += chunk;
+            if (postData.length > maxData) {
+                postData = '';
+                this.pause();
+                res.writeHead(413);
+                response.end('Too large');
+            }
+            else
+                postData += chunk;
         }).on('end', function () {
+            if (!postData) {
+                response.end();
+                return;
+            }
+
             var postDataObject = querystring.parse(postData);
             console.log('User posted: \n' + postData);
             res.end('You posted: \n' + util.inspect(postDataObject));
